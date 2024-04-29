844756fffd13f12387ed256e0f5b163cfd24a0d1
Jenna Nguyen <jennanguyen@Shawns-MacBook-Pro-2.local>
2024-04-29 02:08:18 -0500

    added working user login page and auth

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'Assignment3App-jn4gz';
+
+//   posts: { content: string, date: Date }[] = [];
+
+//   addPost(newPost: { content: string, date: Date }) {
+//     this.posts.push(newPost);
+//   }
+// }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Assignment4App-jn4gz';
+
+  isLoggedIn: boolean = false;
+  displayRegisterForm: boolean = false;
+  displayLoginForm: boolean = false;
+
+  onLoginSuccess(event: boolean) {
+    this.isLoggedIn = event;
+  }
+
+  toggleRegisterForm() {
+    this.displayRegisterForm = !this.displayRegisterForm;
+  }
+
+  toggleLoginForm() {
+    this.displayLoginForm = !this.displayLoginForm;
+  }  
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  error: string = '';
+
+  @Output() loginSuccess: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login() {
+    this.http.post<any>('http://localhost:3000/login', { email: this.email, password: this.password })
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          // emit login success event
+          this.loginSuccess.emit(true);
+        },
+        error: (error) => {
+          // handle login error
+          this.error = error.error.message;
+        }
+      });
+  }
+}
